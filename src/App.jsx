f7aa79d9aceb7397c6e175bb135e69f54eacdf2d
HugoJuliano <hugojuliano@hotmail.com.br>
2024-10-10 01:16:07 -0300

    primeiro

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './Header'
+import Meme from './Meme'
+import './styles.css'
+import memesData from './memesData.js'
+
+function App() {
+
+  return (
+<div>
+  <Header />
+  <Meme />
+</div>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header () {
+    return (
+<header className="header">
+<img className="header-image" src="./images/troll-face.png" alt="imagem de troll" />
+<h2 className="header-title">Meme Generator</h2>
+<h4 className="header-project">React Course - Project 3</h4>
+</header>
+);
+
+}
